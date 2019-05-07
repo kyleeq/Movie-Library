5521bb0a6442b43e7fa2d2c8e2c6e623db10160b
unknown <kylee.schreibs2@yahoo.com>
2019-05-07 15:39:46 -0500

    create migrations and add MoviesController with DbContext

--- MovieLibrary/Migrations/201905072033298_retry.Designer.cs
+++ MovieLibrary/Migrations/201905072033298_retry.Designer.cs
@@ -7,13 +7,13 @@ namespace MovieLibrary.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class addIdtoMovieModel : IMigrationMetadata
+    public sealed partial class retry : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addIdtoMovieModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(retry));
         
         string IMigrationMetadata.Id
         {
-            get { return "201905072016595_addIdtoMovieModel"; }
+            get { return "201905072033298_retry"; }
         }
         
         string IMigrationMetadata.Source
---
+++ MovieLibrary/Migrations/201905072033298_retry.cs
@@ -0,0 +1,28 @@
+namespace MovieLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class retry : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.String(),
+                        DirectorName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
