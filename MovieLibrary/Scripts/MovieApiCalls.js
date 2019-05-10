941a1495cbb99a963e8f96da95ed9834bd7b70d9
unknown <kylee.schreibs2@yahoo.com>
2019-05-10 01:37:00 -0500

    add Post functionality in MovieApiCa;;.js

--- MovieLibrary/Scripts/MovieApiCalls.js
+++ MovieLibrary/Scripts/MovieApiCalls.js
@@ -1,15 +1,21 @@
-﻿function loadJSON(){
-    $(function () {
-        $.get("http://localhost:49206/api/movies", function (data, status) {
-            if (status == "success") {
-
-                var jsonObj = JSON.parse(data);
-                return jsonObj;
-            }
-        })
-        return "failed";
-    }
-
-function updateJSON() {
-            var data_file = "http://localhost:49206/api/movies";
+﻿function loadJSON() {
+    $.get("http://localhost:49206/api/movies", function (data, status) {
+        if (status == "success") {
+            var jsonObj = JSON.parse(data);
+            return jsonObj;
         }
+    })
+    return "failed";    
+}
+
+function createJSON(title, genre, directorName) {
+    $.post("http://localhost:49206/api/movies",
+        {
+            Title: title,
+            Genre: genre,
+            DirectorName: directorName
+        },
+        function (status) {
+            return status;
+        })
+}
